5c99c1c4b00cc0230fe8fde12bc5e9ba4cfabf3c
Antoine Toulme <antoine@lunar-ocean.com>
2016-08-27 16:13:30 -0700

    enable tests and add to README

--- src/test/scala/io/tmio/embeddedcassandra/EmbeddedCassandraSpec.scala
+++ src/test/scala/io/tmio/embeddedcassandra/EmbeddedCassandraSpec.scala
@@ -4,17 +4,14 @@ import org.scalatest.{FlatSpec, Matchers}
 
 import com.datastax.driver.core.Session
 
-/**
-  * Created by lb on 8/19/16.
-  */
 class EmbeddedCassandraSpec extends FlatSpec with Matchers with EmbeddedCassandra {
 
   "An embedded Cassandra server" should "take a body" in {
     withRunningCassandra() {
       runInSession((session : Session) => {
-//        val rs = session.execute("select * from system.local")
-//        rs.all()
-    })
+        val rs = session.execute("select * from system.local")
+        rs.all()
+      })
     }
 
   }
